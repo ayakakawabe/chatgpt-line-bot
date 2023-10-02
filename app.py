66f85d01457614bc78ac37780c84b735bdc294a3
Ayaka Kawabe <kawabe.ayaka@icloud.com>
2023-10-02 20:07:31 +0900

    chatGPTでの返信に変更

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import openai
 from flask import Flask, request, abort
 
 from linebot import (
@@ -16,6 +17,35 @@ load_dotenv()
 
 CHANNEL_ACCESS_TOKEN=os.environ["CHANNEL_ACCESS_TOKEN"]
 CHANNEL_SECRET=os.environ["CHANNEL_SECRET"]
+
+OPENAI_API_KEY=os.environ["OPENAI_API_KEY"]
+openai.api_key=OPENAI_API_KEY
+
+messages_log = [
+            {"role": "system", "content": "you're 20, UChicago student, and user's friend. you reply short sentences include abbreviation like OMG and lol, don't use difficult words. Because user's an uni student in Japan and not good as English."}
+            ]
+
+def response_message_by_chatGPT(user_message):
+    messages_log.append({"role": "user", "content": user_message})
+
+    # API呼び出し
+    try:
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages_log
+        )
+    except (openai.error.RateLimitError, openai.error.InvalidRequestError) as error:
+        return {"type":"error","message":error.user_message}
+
+    # 応答の取得 / 表示 / messagesへの追加
+    response_message = completion["choices"][0]["message"]["content"]
+    print("OpenAIの応答です： " + response_message)
+    messages_log.append({"role": "assistant", "content": response_message})
+    return {"type":"response","message":response_message}
+
+    # token数の取得と表示
+    #tokens = completion["usage"]["total_tokens"]
+    #print("今回のToken数： " + str(tokens))
 
 app = Flask(__name__)
 
@@ -46,10 +76,12 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    response_data=response_message_by_chatGPT(event.message.text)
+    response_message=response_data["message"]
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=response_message))
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
